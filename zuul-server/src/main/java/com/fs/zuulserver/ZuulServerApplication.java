2304828925f033233b1da80bf067d830dc3fbd06
fda <fangdean@yeah.net>
2018-11-08 16:15:48 +0800

    服务修改

--- zuul-server/src/main/java/com/fs/zuulserver/ZuulServerApplication.java
+++ zuul-server/src/main/java/com/fs/zuulserver/ZuulServerApplication.java
@@ -3,11 +3,12 @@ package com.fs.zuulserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulServer
+@EnableZuulProxy
 public class ZuulServerApplication {
 
     public static void main(String[] args) {
